a1a0bbe7a0fe42b0580eb5e95799430575f1e834
kenyialcala <kenyialcala3@gmail.com>
2025-06-21 14:49:55 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function contactarPorWsp(producto) {
-  const numero = "5491234567890"; // Cambia este número por el tuyo
-  const mensaje = `Hola, estoy interesado/a en comprar: ${producto}. ¿Está disponible?`;
-  const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
-  window.open(url, '_blank');
-}
